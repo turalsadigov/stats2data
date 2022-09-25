575fa2c3195e04a434cae0c6d4ac40b698c18ee3
Tural Sadigov <turalsadikov@gmail.com>
2022-09-25 13:56:32 -0400

    county description

---
+++ R/county.R
@@ -0,0 +1,10 @@
+#' United States Counties
+#'
+#' Data for 3142 counties in the United States
+#' 
+#' @format A tibble with 3,142 rows and 15 variables:
+#' 
+#' \describe{
+#' }
+#' @source \url{https://www.openintro.org/data/index.php?data=county}
+"county"
