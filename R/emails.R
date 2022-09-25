4e8a201481396173646f3cd579180354fa26247d
Tural Sadigov <turalsadikov@gmail.com>
2022-09-25 14:01:59 -0400

    emails description

---
+++ R/emails.R
@@ -0,0 +1,10 @@
+#' Data frame representing information about a collection of emails
+#'
+#' These data represent incoming emails for the first three months of 2012 for an email account (see Source).
+#' 
+#' @format A tibble with 3,921 rows and 121 variables:
+#' 
+#' \describe{
+#' }
+#' @source \url{https://www.openintro.org/data/index.php?data=email}
+"emails"
