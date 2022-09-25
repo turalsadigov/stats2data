52222f02103ad52f6aa1af36fd47fffc172e8be2
Tural Sadigov <turalsadikov@gmail.com>
2022-09-25 14:33:55 -0400

    more description

---
+++ R/nc_births.R
@@ -0,0 +1,11 @@
+#' North Carolina births, 1000 cases
+#'
+#' Data for 3142 counties in the United States
+#' 
+#' @format A tibble with 1000 rows and 13 variables:
+#' 
+#' \describe{
+#' In 2004, the state of North Carolina released to the public a large data set containing information on births recorded in this state. This data set has been of interest to medical researchers who are studying the relation between habits and practices of expectant mothers and the birth of their children. This is a random sample of 1,000 cases from this data set.
+#' }
+#' @source \url{https://www.openintro.org/data/index.php?data=ncbirths}
+"nc_births"
---
+++ R/smoking.R
@@ -0,0 +1,11 @@
+#' UK Smoking Data
+#'
+#' Data for 3142 counties in the United States
+#' 
+#' @format A tibble with 1,691 rows and 12 variables:
+#' 
+#' \describe{
+#' Survey data on smoking habits from the UK. The data set can be used for analyzing the demographic characteristics of smokers and types of tobacco consumed.
+#' }
+#' @source \url{https://www.openintro.org/data/index.php?data=smoking}
+"smoking"
