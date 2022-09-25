0afe78acf79f727e7395529fcdad8d8d857d6dea
Tural Sadigov <turalsadikov@gmail.com>
2022-09-25 14:07:24 -0400

    delays description

---
+++ R/departure_delays.R
@@ -0,0 +1,10 @@
+#' Departure delays in Syracuse Hancock International Airport in 2019
+#'
+#' Three major airlines: United Airlines, Delta Airlines and American Airlines
+#' 
+#' @format A tibble with 3,161 rows and 6 variables:
+#' 
+#' \describe{
+#' }
+#' @source \url{https://www.transtats.bts.gov/}
+"departure_delays"
