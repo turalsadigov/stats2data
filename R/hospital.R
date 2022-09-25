29ced834dac0c6a3d69eea60e0ef6dee42a295bd
Tural Sadigov <turalsadikov@gmail.com>
2022-09-25 14:16:32 -0400

    hospital description

---
+++ R/hospital.R
@@ -0,0 +1,12 @@
+#' In-hospital deaths from myocardial infarction with ST elevation (STEMI)
+#' 9 hospitals in the Providence Health system in Oregon.
+#` Study period : from 2002 to 2003.
+#` 913 STEMI patients treated, of whom 105 died (in-hospital deaths) from myocardial infarction with ST elevation (STEMI).
+#` Each patient was assigned a Thrombolysis in Myocardial Infarction Risk Scores
+#' 
+#' @format A tibble with 13 rows and 4 variables:
+#' 
+#' \describe{
+#' }
+#' @source \url{https://stat2-critical-look.netlify.app/the-bootstrap-method.html}
+"hospital"
