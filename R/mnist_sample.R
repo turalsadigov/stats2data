fb0dbc879063ffb38cba16bca714cacb082b507f
Tural Sadigov <turalsadikov@gmail.com>
2022-11-24 17:45:53 -0500

    mnist

---
+++ R/mnist_sample.R
@@ -0,0 +1,11 @@
+#' Sample from famous MNIST data
+#'
+#' Data contains simple random sample of 5000 images that put into rows. The first column is the label.
+#' 
+#' 
+#' @format A tibble with 5,000  rows and 785 variables:
+#' 
+#' \describe{
+#' }
+#' @source \url{http://yann.lecun.com/exdb/mnist/}
+"mnist_sample"
