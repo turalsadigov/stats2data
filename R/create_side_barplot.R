e4e2a84ba09042b9336f45cf7aad674e9d65d34c
Tural Sadigov <turalsadikov@gmail.com>
2022-11-26 23:44:49 -0500

    barplot function updated

--- R/create_side_barplot.R
+++ R/create_side_barplot.R
@@ -1,14 +1,15 @@
-#' Creates a 'sorted' and flipped barplot for a categorical variable in dataframe.
-#'
+#' Create a barplot
+#' 
+#' This function creates a 'sorted' and flipped barplot 
+#' for a categorical variable in dataframe. It assumes that 
+#' the first argument is a tibble and the second argument
+#' is a categorical variable (specifically a factor type)
+#' from that tibble
 #' 
 #' @param df tibble
 #' @param cat_var categorical variable
 #' @return side, sorted, barplot
 #' @export
-#' @examples
-
-
-
 create_side_barplot <- function(df, cat_var){
   df %>% 
     dplyr::count(!!enquo(cat_var), sort = TRUE) %>% 
